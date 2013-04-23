4f61906916009c6451023727fdfd6ffd3f5cab42
Christian Andersson <dhca82@gmail.com>
2013-04-23 14:24:02 +0200

    Added Gallery page type

---
+++ src/Vaultopia.Web/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using Vaultopia.Web.Models.Pages;
+
+namespace Vaultopia.Web.Controllers {
+    public class GalleryController : PageController<GalleryPage> {
+        public ActionResult Index(GalleryPage currentPage) {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
---
+++ src/Vaultopia.Web/Models/Pages/GalleryPage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace Vaultopia.Web.Models.Pages {
+    [ContentType(DisplayName = "Gallery", GUID = "f8b2c441-9e8c-4173-9d47-559e8c2a2fe9", Description = "")]
+    public class GalleryPage : PageData {
+        
+        [CultureSpecific]
+        [Required]
+        [Display(Name = "Heading", GroupName = SystemTabNames.Content, Order = 1)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Introduction", GroupName = SystemTabNames.Content, Order = 2)]
+        [UIHint(UIHint.Textarea)]
+        public virtual string Introduction { get; set; }
+    }
+}
