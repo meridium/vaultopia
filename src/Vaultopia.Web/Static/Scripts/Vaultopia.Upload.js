16bb3d1ad0a1a6ed5f7c6a0321020e331d223ac2
Christian Andersson <dhca82@gmail.com>
2013-04-24 16:20:38 +0200

    Markup and css for gallery

---
+++ src/Vaultopia.Web/Static/Scripts/Vaultopia.Upload.js
@@ -0,0 +1,19 @@
+﻿Vaultopia.Upload = function () {
+    var $container;
+
+
+    var openDialog = function () {
+        $.get("upload", function (data) {
+            $('body').prepend(data);
+        });
+    };
+
+    var init = function () {
+        $container = $('#upload');
+        openDialog();
+    };
+
+    return {
+        init: init
+    };
+}()
