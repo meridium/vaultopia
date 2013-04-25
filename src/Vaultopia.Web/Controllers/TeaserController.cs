dcc526cfe6a78820348102c5a16f28ee49c9e6b0
Robert Blomqvist <robert@meridium.se>
2013-04-25 17:00:55 +0200

    Added ImageVault property to teaser blocks and views

--- src/Vaultopia.Web/Controllers/TeaserController.cs
+++ src/Vaultopia.Web/Controllers/TeaserController.cs
@@ -1,13 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Mvc;
+using ImageVault.Client;
+using ImageVault.Client.Query;
+using ImageVault.Common.Data;
 using Vaultopia.Web.Models.Blocks;
 
 namespace Vaultopia.Web.Controllers {
     public class TeaserController : BlockController<TeaserBlock> {
         private readonly IContentRepository _repository;
+        private readonly Client _client;
 
         public TeaserController(/*IContentRepository repository*/) {
             
@@ -15,13 +20,15 @@ namespace Vaultopia.Web.Controllers {
             var repository = ServiceLocator.Current.GetInstance<IContentRepository>();
 
             _repository = repository;
+            _client = ClientFactory.GetSdkClient();
         }
 
         public override ActionResult Index(TeaserBlock currentBlock) {
 
             var model = new TeaserBlockViewModel {
                 Block = currentBlock,
-                Page = _repository.Get<PageData>(currentBlock.TeaserLink)
+                Page = _repository.Get<PageData>(currentBlock.TeaserLink),
+                WebMedia = _client.Load<WebMedia>(currentBlock.TeaserImage.Id).Resize(width: 280, height: 180, resizeMode: ResizeMode.ScaleToFill).Single()
             };
 
             return PartialView(model);
--- src/Vaultopia.Web/Models/Blocks/TeaserBlockViewModel.cs
+++ src/Vaultopia.Web/Models/Blocks/TeaserBlockViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using EPiServer.Core;
+using ImageVault.Common.Data;
 
 namespace Vaultopia.Web.Models.Blocks {
     public class TeaserBlockViewModel {
         public TeaserBlock Block { get; set; }
         public PageData Page { get; set; }
+        public WebMedia WebMedia { get; set; }
     }
 }
