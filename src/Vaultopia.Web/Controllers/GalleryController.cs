de35314bbd37d05d23a2e7508ae5c82371a60af5
Christian Andersson <dhca82@gmail.com>
2013-04-26 15:58:56 +0200

    Merged master

--- src/Vaultopia.Web/Controllers/GalleryController.cs
+++ src/Vaultopia.Web/Controllers/GalleryController.cs
@@ -1,22 +1,9 @@
 ﻿using System.Web.Mvc;
-<<<<<<< HEAD
 using EPiServer.Web.Mvc;
-=======
->>>>>>> 19ffee01538cc64785005cd6f090c57151d56b4c
 using Vaultopia.Web.Models.Pages;
 using Vaultopia.Web.Models.ViewModels;
 
 namespace Vaultopia.Web.Controllers {
-<<<<<<< HEAD
-    public class GalleryController : PageController<GalleryPage> {
-        
-        public GalleryController() {
-            //var client =  ClientFactory.GetSdkClient();
-        }
-
-        public ActionResult Index(GalleryPage currentPage) {
-            return View(currentPage);
-=======
     public class GalleryController : PageControllerBase<GalleryPage> {
         public ActionResult Index(GalleryPage currentPage) {
             /* Implementation of action. You can create your own view model class that you pass to the view or
@@ -25,7 +12,7 @@ namespace Vaultopia.Web.Controllers {
             var viewModel = new PageViewModel<GalleryPage>(currentPage);
 
             return View(viewModel);
->>>>>>> 19ffee01538cc64785005cd6f090c57151d56b4c
+
         }
         public ActionResult Upload() {
             return PartialView("Upload");
@@ -41,6 +28,9 @@ namespace Vaultopia.Web.Controllers {
             //var vault = client.Query<Vault>().Where(v => v.).FirstOrDefault();
 
             return Content("uploaded");
+        }
+                public GalleryController() {
+            //var client =  ClientFactory.GetSdkClient();
         }
     }
 }
