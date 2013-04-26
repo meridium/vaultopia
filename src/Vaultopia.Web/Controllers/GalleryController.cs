ace76ecb34651d1c20fde2322437e36131ddac0b
Christian Andersson <dhca82@gmail.com>
2013-04-26 15:34:14 +0200

    Upload dialog wip

--- src/Vaultopia.Web/Controllers/GalleryController.cs
+++ src/Vaultopia.Web/Controllers/GalleryController.cs
@@ -5,11 +5,27 @@ using Vaultopia.Web.Models.Pages;
 namespace Vaultopia.Web.Controllers {
     public class GalleryController : PageController<GalleryPage> {
         
+        public GalleryController() {
+            //var client =  ClientFactory.GetSdkClient();
+        }
+
         public ActionResult Index(GalleryPage currentPage) {
             return View(currentPage);
         }
-        public ActionResult Upload(GalleryPage currentPage) {
+        public ActionResult Upload() {
             return PartialView("Upload");
+        }
+        public ActionResult Save() {
+            return Content("ok");
+        }
+
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBaseModelBinder file) {
+
+            //var vault = client.Query<Vault>().Where(v => v.).FirstOrDefault();
+
+            return Content("uploaded");
         }
     }
 }
