76b97d2da86306a9a07157f2ceec3fd8ebe01a33
Christian Andersson <dhca82@gmail.com>
2013-05-14 11:41:00 +0200

    Thumbnail hover effects

--- src/Vaultopia.Web/Static/Scripts/Vaultopia.Gallery.js
+++ src/Vaultopia.Web/Static/Scripts/Vaultopia.Gallery.js
@@ -23,39 +23,90 @@
                         if ($images.find('li').length <= 16) {
                             $('#gallery-paging-action').remove();
                         }
+                        
+                        registerHoverEvents();
                     });
-
                 }
             });
         });
        
         $(document).on('click', '#gallery .metadata-anchor', function(e) {
             e.preventDefault();
+            e.stopPropagation();
 
             var $that = $(this);
+            var $section;
+
+            $('.metadata-anchor').not(this).fadeOut(100);
 
             if ($that.closest('li').find('#metadata').length > 0) {
-                $('#metadata').remove();
+                $('#metadata').fadeOut(100, function () { $(this).remove(); });
                 return;
             } else {
-                $('#metadata').remove();
+                $('#metadata').fadeOut(100, function () { $(this).remove(); });
                 $.ajax({
                     url: 'ShowMetaData',
                     data: { imageId: $that.closest('li').attr('data-image-id') },
-                    success: function (data) {
-                        $that.after(data);
+                    beforeSend: function() {
+                        $section = $('<section id="metadata" class="loading">' +
+                                       '<span class="arrow sprite"></span>' +
+                                     '</section>');
+                        $that.after($section);
+                    },
+                    success: function (html) {
+                        var $html = $(html);
+                        
+                        $html.hide();
+                        
+                        $section.removeClass('loading').find('span').after($html);
+                        $html.fadeIn(100);
+
+                        
+
                     }
                 });
             }
-
-
         });
 
+        $(document).on('click', '#metadata', function(e) {
+            e.stopPropagation();
+        });
+        $(document).on('click', 'body', function(e) {
+            removeMetaDataOverlay();
+        });
+
+        registerHoverEvents();
+
+    };
+
+    var registerHoverEvents = function() {
+        $('#gallery li').hoverIntent(function () {
+            $(this).addClass('hover');
+            $(this).find('img').fadeTo(100, .3);
+            $(this).find('.enlarge').fadeIn(100);
+            $(this).find('.metadata-anchor').fadeIn(100);
+        }, function () {
+            $(this).removeClass('hover');
+            $(this).find('img').fadeTo(100, 1);
+            $(this).find('.enlarge').fadeOut(100);
+            if ($(this).find('#metadata').length == 0) {
+                $(this).find('.metadata-anchor').fadeOut(100);
+            }
+        });
+    };
+
+    var removeMetaDataOverlay = function() {
+        $('.metadata-anchor').fadeOut(100);
+        $('#metadata').fadeOut(100, function () {
+            $(this).remove();
+        });
     };
 
     var init = function () {
         $container = $('#gallery');
         registerEvents();
+
+
     };
 
     return {
