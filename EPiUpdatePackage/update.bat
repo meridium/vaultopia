94e319c312bae62367e08f984965fc8d45b8dcfb
Daniel <daniel@meridium.se>
2014-10-17 15:36:55 +0200

    added

--- EPiUpdatePackage/update.bat
+++ EPiUpdatePackage/update.bat
@@ -2,6 +2,6 @@
  if '%1' ==''  (
  echo  USAGE: %0  web application path "[..\episerversitepath or c:\episerversitepath]" 
 	) else (
-epideploy.exe  -a sql -s "%~f1"  -p "EPiServer.CMS.Core.7.13.3\epiupdates\*" -c "EPiServerDB"
+epideploy.exe  -a sql -s "%~f1"  -p "EPiServer.CMS.Core.7.14.1\epiupdates\*" -c "EPiServerDB"
 ) 
 
