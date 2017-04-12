a6d58ec17d7d0a8a8c952aac1eae48ec43d37b24
Mathias Olsson <mathias@meriworks.se>
2017-04-12 08:47:24 +0200

    Added files to nuspec

---
+++ Vaultopia.Web/obj/Release/Package/PackageTmp/Static/Scripts/Global.js
@@ -0,0 +1,20 @@
+﻿var Vaultopia = {};
+
+$(document).ready(function () {
+
+    //Responsive navigation
+    $('#toggle-nav').click(function (e) {
+        e.preventDefault();
+        if ($('#global-nav').is(':visible')) {
+            $('#global-nav').hide();
+            $(this).removeClass('open');
+        } else {
+            $('#global-nav').show();
+            $(this).addClass('open');
+        }
+    });
+
+
+    $('#inspiration').find('.image').fancybox();
+
+});
