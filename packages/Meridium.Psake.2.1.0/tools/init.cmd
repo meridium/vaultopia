bc7d3ecdb0015582413a4fe69bce5b3d86194e2a
Robert Blomqvist <robert@meriworks.se>
2018-06-26 16:18:14 +0200

    Updated Episerver.CMS.UI to 9.6.2 to fix Chrome bug

--- packages/Meridium.Psake.2.1.0/tools/init.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo %windir%
-for /F "tokens=*" %%A in ('dir ..\packages\Meridium.Psake.* /O-N /B') do (
-	%windir%\syswow64\windowspowershell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '..\packages\%%A\tools\initialize.ps1' %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
-	goto :loopend
-)
-
-:loopend
